affd755e76abcce7db6fbb601a3b787562b26baf
Alexander Gottwald <alexander.gottwald@gmail.com>
2017-10-21 13:39:40 +0200

    Discover mods from installed mods/modname/modname.jar files

--- src/main/java/org/gotti/wurmunlimited/modloader/ModLoaderShared.java
+++ src/main/java/org/gotti/wurmunlimited/modloader/ModLoaderShared.java
@@ -4,14 +4,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.URI;
 import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -58,10 +64,57 @@ public abstract class ModLoaderShared<T extends Versioned> {
 	protected abstract void modcommInit();
 	protected abstract void preInit();
 	protected abstract void init();
-
-	public List<T> loadModsFromModDir(Path modDir) throws IOException {
+	
+	/**
+	 * Find installed mods and read the properties.
+	 * <p>
+	 * It loads the properties in the following order with later entries overriding earlier ones
+	 * <ul>
+	 * <li>Properties from mods/modname/modname.jar!META-INF/org.gotti.wurmunlimited.modloader/modname.properties</li>
+	 * <li>Properties from mods/modname.properties</li>
+	 * <li>Properties from mods/modname.config</li>
+	 * </ul>
+	 * <p>
+	 * The mod will be set to ondemand loading if properties from the jar file exist but no .properties file. This will
+	 * enable support mods like httpserver to load without a .properties file when needed. Mods can set depend.ondemand=false
+	 * in the properties file in the jar to force loading the mod. Setting depend.ondemand=false in modname.config will then
+	 * disable the mod per user request, although removing the files of the mod would probably be a better option.
+	 * <p>
+	 *
+	 * @param modDir mods folder
+	 * @return
+	 * @throws IOException
+	 */
+	private List<ModInfo> discoverMods(Path modDir) throws IOException {
 		final List<ModInfo> unorderedMods = new ArrayList<>();
+		final Set<String> handled = new HashSet<>();
 		
+		try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(modDir, "*.properties")) {
+			for (Path modInfo : directoryStream) {
+				String modName = modInfo.getFileName().toString().replaceAll("\\.properties$", "");
+				Path modJar = modDir.resolve(modName).resolve(modName + ".jar");
+				ModInfo mod = loadModFromInfo(modName, modInfo, modJar);
+				unorderedMods.add(mod);
+				handled.add(modName);
+			}
+		}
+		
+		try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(modDir, path -> Files.isDirectory(path) && !handled.contains(path.getFileName().toString()))) {
+			for (Path modInfo : directoryStream) {
+				String modName = modInfo.getFileName().toString();
+				Path modJar = modDir.resolve(modName).resolve(modName + ".jar");
+				if (Files.exists(modJar)) {
+					ModInfo mod = loadModFromInfo(modName, null, modJar);
+					unorderedMods.add(mod);
+					handled.add(modName);
+				}
+			}
+		}
+		
+		return unorderedMods;
+	}
+	
+	public List<T> loadModsFromModDir(Path modDir) throws IOException {
 		final String version = this.getClass().getPackage().getImplementationVersion();
 		logger.info(String.format("ModLoader version %1$s", version));
 		
@@ -71,21 +124,17 @@ public abstract class ModLoaderShared<T extends Versioned> {
 		}
 		
 		String steamVersion = getSteamVersion();
-
-		try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(modDir, "*.properties")) {
-			for (Path modInfo : directoryStream) {
-				logger.log(Level.INFO, "Loading " + modInfo.toString());
-				try (EarlyLoadingChecker c = EarlyLoadingChecker.init(modInfo.getFileName().toString().replaceAll("\\.properties$", ""), "load")) {
-					ModInfo mod = loadModFromInfo(modInfo);
-					
-					mod.getProperties().put("steamVersion", steamVersion);
-					unorderedMods.add(mod);
-				}
-			}
-		}
+		
+		// Discover installed (and possibly enabled) mods from modDir
+		final List<ModInfo> unorderedMods = discoverMods(modDir);
 		
 		ModInstanceBuilder<T> entryBuilder = new ModInstanceBuilder<T>(modClass);
-		List<Entry> mods = new DependencyResolver<ModInfo>().provided(Collections.singleton(modLoaderProvided)).order(unorderedMods).stream().map(modInfo -> new Entry(entryBuilder.createModInstance(modInfo), modInfo.getProperties(), modInfo.getName())).collect(Collectors.toList());
+		List<Entry> mods = new DependencyResolver<ModInfo>().provided(Collections.singleton(modLoaderProvided)).order(unorderedMods).stream().map(modInfo -> {
+			try (EarlyLoadingChecker c = EarlyLoadingChecker.init(modInfo.getName(), "load")) {
+				modInfo.getProperties().put("steamVersion", steamVersion);
+				return new Entry(entryBuilder.createModInstance(modInfo), modInfo.getProperties(), modInfo.getName());
+			}
+		}).collect(Collectors.toList());
 		
 		// new style mods with initable will do configure, preInit, init
 		mods.stream().filter(modEntry -> (modEntry.mod instanceof Initable || modEntry.mod instanceof PreInitable) && modEntry.mod instanceof Configurable).forEach(modEntry -> {
@@ -139,16 +188,56 @@ public abstract class ModLoaderShared<T extends Versioned> {
 		return mods.stream().map(modEntry -> modEntry.mod).collect(Collectors.toList());
 	}
 	
-	public ModInfo loadModFromInfo(Path modInfo) throws IOException {
+	/**
+	 * Load the default properties from mods/modname/modname.jar!META-INF/org.gotti.wurmunlimited.modloader/modname.properties
+	 *
+	 * @param modName Modname
+	 * @param jarFile Jar-file of the mod
+	 * @return Properties
+	 * @throws IOException
+	 */
+	private Properties loadDefaultPropertiesFromJar(String modName, Path jarFile) throws IOException {
+		try (FileSystem fs = FileSystems.newFileSystem(URI.create("jar:" + jarFile.toUri()), new HashMap<>())) {
+			Path propsFile = fs.getPath("/META-INF/" + ModLoaderShared.class.getPackage().getName() + "/" + modName + ".properties");
+			if (Files.exists(propsFile)) {
+				logger.log(Level.INFO, "Loading " + jarFile.toString());
+				try (InputStream inputStream = Files.newInputStream(propsFile)) {
+					Properties properties = new Properties();
+					properties.load(inputStream);
+					return properties;
+				}
+			}
+		} catch (IOException e) {
+			logger.log(Level.WARNING, e.getMessage(), e);
+		}
+		return new Properties();
+	}
+	
+	/**
+	 * Load mod properties from .properties and .config files
+	 * @param modName Modname
+	 * @param modInfo properties file
+	 * @param jarFile jar file
+	 * @return Mod properties
+	 * @throws IOException
+	 */
+	private ModInfo loadModFromInfo(String modName, Path modInfo, Path jarFile) throws IOException {
 		Properties properties = new Properties();
-
-		try (InputStream inputStream = Files.newInputStream(modInfo)) {
-			properties.load(inputStream);
+		if (jarFile != null && Files.exists(jarFile)) {
+			if (modInfo == null || !Files.exists(modInfo)) {
+				properties.put("depend.ondemand", "true");
+			}
+			properties.putAll(loadDefaultPropertiesFromJar(modName, jarFile));
 		}
 
-		String modname = modInfo.getFileName().toString().replaceAll("\\.properties$", "");
-		
-		Path configFile = Paths.get("mods", modname + ".config");
+		if (modInfo != null) {
+			logger.log(Level.INFO, "Loading " + modInfo.toString());
+			try (InputStream inputStream = Files.newInputStream(modInfo)) {
+				properties.load(inputStream);
+			}
+		}
+
+		Path configFile = Paths.get("mods", modName + ".config");
 		if (Files.exists(configFile)) {
 			try (InputStream inputStream = Files.newInputStream(configFile)) {
 				logger.log(Level.INFO, "Loading " + configFile.toString());
@@ -156,9 +245,13 @@ public abstract class ModLoaderShared<T extends Versioned> {
 			}
 		}
 		
-		return new ModInfo(properties, modname);
+		return new ModInfo(properties, modName);
 	}
 	
+	/**
+	 * Get the server version as announced on steam
+	 * @return Steam game version
+	 */
 	private String getSteamVersion() {
 		final ClassPool classPool = HookManager.getInstance().getClassPool();
 		
